ff05315d5a9d8e6d480ca9c2c996da0e2fe1f322
Sebastian Riedel <s@kraih.com>
2022-06-22 00:39:12 +0200

    Add some reference documentation

--- src/util.ts
+++ src/util.ts
@@ -15,8 +15,14 @@ const XML_UNESCAPE: Record<string, string> = {
   '#39': "'"
 };
 
+/**
+ * Safe string that should not be escaped.
+ */
 export class SafeString extends String {}
 
+/**
+ * CSS unescape string.
+ */
 export function cssUnescape(value: string): string {
   return value.replaceAll('\\n', '').replace(/\\([0-9a-fA-F]{1,6})\s?/g, cssUnescapeReplace);
 }
@@ -25,10 +31,16 @@ function cssUnescapeReplace(value: string): string {
   return String.fromCharCode(parseInt(value.replaceAll('\\', ''), 16));
 }
 
+/**
+ * Escape string for use in a regular expression.
+ */
 export function escapeRegExp(string: string) {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 }
 
+/**
+ * Helper function for sticky regex matching.
+ */
 export function stickyMatch(
   stringWithOffset: {offset: number; value: string},
   stickyRegex: RegExp
@@ -39,6 +51,9 @@ export function stickyMatch(
   return match;
 }
 
+/**
+ * XML escape string, but exclude `SafeString` objects.
+ */
 export function xmlEscape(value: string | SafeString): string {
   if (value instanceof SafeString) return value.toString();
   if (typeof value !== 'string') value = `${value}`;
@@ -48,6 +63,9 @@ function xmlEscapeReplace(char: string): string {
   return XML_ESCAPE[char];
 }
 
+/**
+ * XML unescape string.
+ */
 export function xmlUnescape(value: string): string {
   return value.replace(/&(amp|lt|gt|quot|apos|#39);/g, xmlUnescapeReplace);
 }
